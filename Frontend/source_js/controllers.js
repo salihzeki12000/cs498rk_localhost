148c138cb8b76d0714e3ba691528a22ff1101738
Sarah Helm <sghelm2@illinois.edu>
2016-05-06 02:20:03 +0000

    error handling when logging in/signing up

--- Frontend/source_js/controllers.js
+++ Frontend/source_js/controllers.js
@@ -53,6 +53,7 @@ appControllers.controller('ProfileController', ['$scope', '$window', '$http', 'U
 appControllers.controller('LoginController', ['$scope', '$window', '$route', 'Auth', 'Users', function($scope, $window, $route, Auth, Users) {
   $scope.data = "";
   $scope.displayText = "";
+  $('.alert').hide();
 
   $scope.login = function() {
     console.log($scope.user);
@@ -62,7 +63,7 @@ appControllers.controller('LoginController', ['$scope', '$window', '$route', 'Au
       $window.localStorage.setItem('loggedIn', 'true');*/
       $window.location.href = '#/profile';
     }).error(function (data) {
-      console.log("error");
+      $('.alert').show();
     });
   };
 
@@ -70,6 +71,7 @@ appControllers.controller('LoginController', ['$scope', '$window', '$route', 'Au
 
 appControllers.controller('SignupController', ['$scope', '$window', '$route', 'Auth', 'Users', 'User', function($scope, $window, $route, Auth, Users, User) {
   $scope.newUser = "";
+  $('.alert').hide();
   $scope.name ="";
   $scope.signup = function() {
     Users.postSignUp($scope.newUser).success(function (data) {
@@ -82,6 +84,8 @@ appControllers.controller('SignupController', ['$scope', '$window', '$route', 'A
         $window.localStorage.setItem('loggedIn', 'true');*/
         $window.location.href = '#/travellerhost';
         $route.reload();
+    }).error(function(data) {
+      $('.alert').show();
     });
   }
 
